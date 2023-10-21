79fa6d767150075011a19f5d1a6362c5c8037c01
Tanay <tanayverma98@gmail.com>
2023-10-21 19:33:06 +0530

    Handle New Issues submission error

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,9 +1,12 @@
 "use client";
-import { Button, TextArea, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextArea, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller, SubmitHandler } from "react-hook-form";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { AiFillWarning } from "react-icons/ai";
+import axios from "redaxios";
 
 interface IssueForm {
   title: string;
@@ -11,42 +14,56 @@ interface IssueForm {
 }
 const NewIssuePage = () => {
   const { register, control, handleSubmit } = useForm<IssueForm>();
-
   const router = useRouter();
+  const [error, setError] = useState("");
 
   const onSubmit: SubmitHandler<IssueForm> = async (data) => {
-    await fetch("/api/issues", { method: "POST", body: JSON.stringify(data) });
-    router.push("/issues");
+    try {
+      await axios.post("/api/issues", data);
+      router.push("/issues");
+    } catch (error) {
+      setError("An unexpected error occured.");
+    }
   };
 
   return (
-    <form className="max-w-xl space-y-4" onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <label htmlFor="title">Title</label>
-        <TextField.Root>
-          <TextField.Input
-            id="title"
-            placeholder="Title for you issue…"
-            {...register("title")}
-          />
-        </TextField.Root>
-      </div>
-      <div>
-        <label>Description</label>
-        <Controller
-          name="description"
-          control={control}
-          render={({ field: { onChange, onBlur } }) => (
-            <SimpleMDE
-              placeholder="Description for your issue..."
-              onChange={onChange}
-              onBlur={onBlur}
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Icon>
+            <AiFillWarning />
+          </Callout.Icon>
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor="title">Title</label>
+          <TextField.Root>
+            <TextField.Input
+              id="title"
+              placeholder="Title for you issue…"
+              {...register("title")}
             />
-          )}
-        />
-      </div>
-      <Button>Submit New Issue</Button>
-    </form>
+          </TextField.Root>
+        </div>
+        <div>
+          <label>Description</label>
+          <Controller
+            name="description"
+            control={control}
+            render={({ field: { onChange, onBlur } }) => (
+              <SimpleMDE
+                placeholder="Description for your issue..."
+                onChange={onChange}
+                onBlur={onBlur}
+              />
+            )}
+          />
+        </div>
+        <Button>Submit New Issue</Button>
+      </form>
+    </div>
   );
 };
 
