651158b66e111f68f48eee50b18ff20a9c447ea2
Tanay <tanayverma98@gmail.com>
2023-10-28 22:22:48 +0530

    Added dropdown menu when user login

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -5,7 +5,7 @@ import { AiOutlineIssuesClose } from "react-icons/ai";
 import classNames from "classnames";
 import { usePathname } from "next/navigation";
 import { useSession } from "next-auth/react";
-import { Container } from "@radix-ui/themes";
+import { Avatar, Container, DropdownMenu, Text } from "@radix-ui/themes";
 const Navbar = () => {
   const links = [
     { name: "Dashboard", href: "/" },
@@ -15,58 +15,89 @@ const Navbar = () => {
   const { status, data: session } = useSession();
   return (
     <nav className="border-b h-14 mb-5 px-5">
-        <div className="mx-auto flex space-x-6 items-center justify-between h-[100%] max-w-[1136px]">
-          <div className="flex space-x-6 items-center h-full">
-            <Link href="/">
-              <AiOutlineIssuesClose className="text-3xl" />
-            </Link>
-            <ul className="flex space-x-6 h-full ">
-              {links.map((link) => (
-                <li
-                  key={link.href}
+      <div className="mx-auto flex space-x-6 items-center justify-between h-[100%] max-w-[1136px]">
+        <div className="flex space-x-6 items-center h-full">
+          <Link href="/">
+            <AiOutlineIssuesClose className="text-3xl" />
+          </Link>
+          <ul className="flex space-x-6 h-full ">
+            {links.map((link) => (
+              <li
+                key={link.href}
+                className={classNames({
+                  "bg-zinc-900": pathName === link.href,
+                  "bg-none": pathName !== link.href,
+                  "h-full flex items-center rounded-sm p-1": true,
+                })}
+              >
+                <Link
                   className={classNames({
-                    "bg-zinc-900": pathName === link.href,
-                    "bg-none": pathName !== link.href,
-                    "h-full flex items-center rounded-sm p-1": true,
+                    "text-zinc-500 hover:text-zinc-800": pathName !== link.href,
+                    "text-zinc-400 hover:text-white": pathName === link.href,
+                    "transition-colors": true,
                   })}
+                  href={link.href}
                 >
+                  {link.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div
+          className={classNames({
+            "bg-zinc-900": pathName === "/api/auth/signout",
+            "bg-none": pathName !== "/api/auth/signout",
+            "h-full flex items-center rounded-sm p-1": true,
+          })}
+        >
+          {status === "authenticated" && (
+            <DropdownMenu.Root>
+              <DropdownMenu.Trigger>
+                <Avatar
+                  fallback="?"
+                  src={session.user!.image!}
+                  radius="full"
+                  className="cursor-pointer"
+                />
+              </DropdownMenu.Trigger>
+              <DropdownMenu.Content>
+                <DropdownMenu.Label>
+                  <Text size="2">{session.user!.email}</Text>
+                </DropdownMenu.Label>
+                <DropdownMenu.Item>
                   <Link
+                    href="/api/auth/signout"
                     className={classNames({
-                      "text-zinc-500 hover:text-zinc-800": pathName !== link.href,
-                      "text-zinc-400 hover:text-white": pathName === link.href,
+                      "text-zinc-500 hover:text-zinc-800":
+                        pathName !== "/api/auth/signout",
+                      "text-zinc-400 hover:text-white":
+                        pathName === "/api/auth/signout",
                       "transition-colors": true,
                     })}
-                    href={link.href}
                   >
-                    {link.name}
+                    Log Out
                   </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div
-            className={classNames({
-              "bg-zinc-900": pathName === "/api/auth/signout",
-              "bg-none": pathName !== "/api/auth/signout",
-              "h-full flex items-center rounded-sm p-1": true,
-            })}
-          >
+                </DropdownMenu.Item>
+              </DropdownMenu.Content>
+            </DropdownMenu.Root>
+          )}
+          {status === "unauthenticated" && (
             <Link
-              href={`/api/auth/${
-                status === "authenticated" ? "signout" : "signin"
-              }`}
+              href="/api/auth/signin"
               className={classNames({
                 "text-zinc-500 hover:text-zinc-800":
-                  pathName !== "/api/auth/signout",
+                  pathName !== "/api/auth/signin",
                 "text-zinc-400 hover:text-white":
-                  pathName === "/api/auth/signout",
+                  pathName === "/api/auth/signin",
                 "transition-colors": true,
               })}
             >
-              {status === "authenticated" ? "Log Out" : "Log In"}
+              Login
             </Link>
-          </div>
+          )}
         </div>
+      </div>
     </nav>
   );
 };
