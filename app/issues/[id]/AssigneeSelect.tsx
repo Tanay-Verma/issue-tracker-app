5b34e8b9897048ac41dd2e04ecd306635fb61851
Tanay <tanayverma98@gmail.com>
2023-10-30 21:40:48 +0530

    Show toast notifications

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -4,6 +4,7 @@ import { Select, SelectGroup } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
 import Skeleton from "@/components/Skeleton";
 import axios from "redaxios";
+import toast, { Toaster } from "react-hot-toast";
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   const {
@@ -25,27 +26,36 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   if (error) return null;
 
   return (
-    <Select.Root
-      defaultValue={issue.assignedToUserId || "null"}
-      onValueChange={(userId) => {
-        axios.patch(`/api/issues/${issue.id}`, {
-          assignedToUserId: userId !== "null" ? userId:null,
-        });
-      }}
-    >
-      <Select.Trigger placeholder="Assign..." />
-      <Select.Content>
-        <SelectGroup>
-          <Select.Label>Suggestion</Select.Label>
-          <Select.Item value="null">Unassigned</Select.Item>
-          {users?.map((user) => (
-            <Select.Item key={user.id} value={user.id}>
-              {user.name}
-            </Select.Item>
-          ))}
-        </SelectGroup>
-      </Select.Content>
-    </Select.Root>
+    <>
+      <Select.Root
+        defaultValue={issue.assignedToUserId || "null"}
+        onValueChange={async (userId) => {
+          try {
+            await axios.patch(`/api/issues/${issue.id}`, {
+              assignedToUserId: userId !== "null" ? userId : null,
+            });
+            toast.success(`${userId === "null" ? "Unassigned successfully":"Assigned successfully"}`)
+          } catch (error) {
+            toast.error("Changes could not be saved")
+          }
+          
+        }}
+      >
+        <Select.Trigger placeholder="Assign..." />
+        <Select.Content>
+          <SelectGroup>
+            <Select.Label>Suggestion</Select.Label>
+            <Select.Item value="null">Unassigned</Select.Item>
+            {users?.map((user) => (
+              <Select.Item key={user.id} value={user.id}>
+                {user.name}
+              </Select.Item>
+            ))}
+          </SelectGroup>
+        </Select.Content>
+      </Select.Root>
+      <Toaster/>
+    </>
   );
 };
 
