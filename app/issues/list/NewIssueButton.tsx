10a9d1211ba0575f58036f48c22b530a27bcd8d7
Tanay <tanayverma98@gmail.com>
2023-10-30 22:05:16 +0530

    Built FilterIssueStatus component

--- app/issues/list/NewIssueButton.tsx
+++ app/issues/list/NewIssueButton.tsx
@@ -1,13 +1,16 @@
 import { Button } from '@radix-ui/themes'
 import Link from 'next/link'
 import React from 'react'
+import FilterIssueStatus from './FilterIssueStatus'
 
 const NewIssueButton = () => {
   return (
-    <div className="mb-5">
+    <div className="mb-5 flex justify-between">
+        <FilterIssueStatus/>
         <Button>
           <Link href="/issues/new">New Issue</Link>
         </Button>
+
       </div>
   )
 }
