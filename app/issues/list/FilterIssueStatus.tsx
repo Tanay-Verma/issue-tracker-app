51cd32b104018ed1e421337ff9a5577dca3728eb
Tanay <tanayverma98@gmail.com>
2023-11-01 22:13:02 +0530

    Bug fixes in Filtering
    status param would remove orderBy params
    
    the filter box value would show nothing when page was refreshed

--- app/issues/list/FilterIssueStatus.tsx
+++ app/issues/list/FilterIssueStatus.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Status } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import { useRouter } from "next/navigation";
+import { useSearchParams, useRouter } from "next/navigation";
 import React from "react";
 
 const statuses: { label: string; value?: Status }[] = [
@@ -12,18 +12,28 @@ const statuses: { label: string; value?: Status }[] = [
 ];
 const FilterIssueStatus = () => {
   const router = useRouter();
+  const searchParams = useSearchParams();
+
   return (
     <Select.Root
+      defaultValue={searchParams.get("status") ?? 'ALL'}
       onValueChange={(status) => {
-        const searchParam = status === "ALL" ? "" : `?status=${status}`;
-        router.push(`/issues/list${searchParam}`);
+
+        const params = new URLSearchParams();
+        if(status !== 'ALL') params.append("status",status);
+        if(searchParams.get("orderBy")) params.append("orderBy",searchParams.get("orderBy")!)
+        const query = params.size ? `?${params.toString()}` : "";
+        router.push(`/issues/list${query}`);
       }}
     >
       <Select.Trigger placeholder="Filter by status" />
       <Select.Content>
         <Select.Group>
           {statuses.map((status) => (
-            <Select.Item key={status.value ?? "ALL"} value={status.value ?? "ALL"}>
+            <Select.Item
+              key={status.value ?? "ALL"}
+              value={status.value ?? "ALL"}
+            >
               {status.label}
             </Select.Item>
           ))}
