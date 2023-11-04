efb129142eff9af7af5cfcb45fe22b081de4cedc
Tanay <tanayverma98@gmail.com>
2023-11-04 19:19:07 +0530

    Optimize perpormance using react cache

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,12 +1,16 @@
+import authOptions from "@/app/api/auth/[...nextauth]/authOptions";
 import prisma from "@/prisma/client";
+import { getServerSession } from "next-auth";
 import { notFound } from "next/navigation";
+import AssigneeSelect from "./AssigneeSelect";
+import DeleteButton from "./DeleteButton";
 import EditButton from "./EditButton";
 import IssueDetails from "./IssueDetails";
-import DeleteButton from "./DeleteButton";
-import { getServerSession } from "next-auth";
-import authOptions from "@/app/api/auth/[...nextauth]/authOptions";
-import AssigneeSelect from "./AssigneeSelect";
-import { Metadata } from "next";
+import { cache } from "react";
+
+const fetchIssue = cache((issueId: number) =>
+  prisma.issue.findUnique({ where: { id: issueId } })
+);
 
 interface Props {
   params: { id: string };
@@ -14,30 +18,30 @@ interface Props {
 
 const IssueDetailsPage = async ({ params }: Props) => {
   const session = await getServerSession(authOptions);
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) },
-  });
+  const issue = await fetchIssue(parseInt(params.id));
   if (!issue) notFound();
   return (
     <div className="grid grid-cols-1 md:grid-cols-5 gap-4">
       <div className="md:col-span-4">
-        <IssueDetails issue={issue}/>
+        <IssueDetails issue={issue} />
       </div>
-      {session && <div className="flex flex-col gap-4">
-        <AssigneeSelect issue={issue}/>
-        <EditButton issueId={params.id}/>
-        <DeleteButton issueId={params.id}/>
-      </div>}
+      {session && (
+        <div className="flex flex-col gap-4">
+          <AssigneeSelect issue={issue} />
+          <EditButton issueId={params.id} />
+          <DeleteButton issueId={params.id} />
+        </div>
+      )}
     </div>
   );
 };
 
 export default IssueDetailsPage;
 
-export const generateMetadata = async({params}:Props) => {
-  const issue = await prisma.issue.findUnique({where:{id:parseInt(params.id)}});
+export const generateMetadata = async ({ params }: Props) => {
+  const issue = await fetchIssue(parseInt(params.id));
   return {
-    title:`Issue Tracker | ${issue?.title}`,
-    description:`Details of issue ${issue?.id}`
-  }
-}
+    title: `Issue Tracker | ${issue?.title}`,
+    description: `Details of issue ${issue?.id}`,
+  };
+};
--- app/issues/edit/[id]/page.tsx
+++ app/issues/edit/[id]/page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { cache } from "react";
 import prisma from "@/prisma/client";
 import { notFound } from "next/navigation";
 import dynamic from "next/dynamic";
@@ -8,14 +8,16 @@ const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
   loading: () => <IssueFormSkeleton />,
 });
 
+const fetchIssue = cache((issueId: number) =>
+  prisma.issue.findUnique({ where: { id: issueId } })
+);
+
 interface Props {
   params: { id: string };
 }
 
 const EditPage = async ({ params }: Props) => {
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) },
-  });
+  const issue = await fetchIssue(parseInt(params.id));
   if (!issue) notFound();
   return <IssueForm issue={issue} />;
 };
@@ -23,9 +25,7 @@ const EditPage = async ({ params }: Props) => {
 export default EditPage;
 
 export const generateMetadata = async ({ params }: Props) => {
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) },
-  });
+  const issue = await fetchIssue(parseInt(params.id));
   return {
     title: `Issue Tracker | ${issue?.title} | Edit`,
     description: `Edit issue ${issue?.id}`,
